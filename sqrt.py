83051cadcadb769843110ac39c2b5744f12b7327
Breno Souza <cognocoder@gmail.com>
2022-03-08 19:12:38 -0300

    Add script with import and user input

---
+++ sqrt.py
@@ -0,0 +1,9 @@
+
+from math import *
+
+userdata = input("Enter a positive numerical value: ")
+decimal = float(userdata)
+value = sqrt(decimal)
+
+print("The square root of " + str(decimal) + " is " + str(value))
+
